258ae39e19c9b88ea9bddff3d783721f2bfd2519
Jordan Masone <jordan.masone@gfs.com>
2022-12-11 16:38:10 -0500

    Added frontend styling

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,5 @@
+const gamesContainer = document.getElementById("game-container");
+
 async function getData(url) {
     const response = await fetch(url);
     return response.json();
@@ -6,6 +8,40 @@ async function getData(url) {
 async function populatePage() {
     const data = await getData("/api");
     console.log(data);
+    htmlArray = [];
+
+    for (let i = 0; i < data.length; i++) {
+        if (i % 2 !== 0) {
+            htmlArray.push(
+                `
+            <div class="col-5 video-box">
+                <div class="d-flex justify-content-between mt-2">
+                    <h3>${data[i].title}</h3>
+                    <i class="bi ${data[i].multiplayer_ind === 0 ? `bi-person-fill` : `bi-people-fill`} logo-img" style="font-size: 25px;" title=${data[i].multiplayer_ind === 0 ? "Single-Player" : "Multi-Player"}></i>
+                </div>
+                <h6>${data[i].genre}</h6>
+                <p>${data[i].description}</p>
+            </div>
+        </div>`
+            )
+        } else {
+            htmlArray.push(
+                `
+                <div class="row mb-5">
+                <div class="col-5 video-box">
+                    <div class="d-flex justify-content-between mt-2">
+                        <h3>${data[i].title}</h3>
+                        <i class="bi ${data[i].multiplayer_ind === 0 ? `bi-person-fill` : `bi-people-fill`} logo-img" style="font-size: 25px;" title=${data[i].multiplayer_ind === 0 ? "Single-Player" : "Multi-Player"}></i>
+                    </div>
+                    <h6>${data[i].genre}</h6>
+                    <p>${data[i].description}</p>
+                </div>
+                <div class="col-2"></div>`
+            )
+        }
+    }
+
+    gamesContainer.innerHTML = htmlArray.join("");
 }
 
 populatePage();
