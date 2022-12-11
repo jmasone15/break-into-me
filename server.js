88b8d4232fc83fddcf3a95d7167690564ac36eaf
Jordan Masone <jordan.masone@gfs.com>
2022-12-11 10:43:26 -0500

    Added static files.

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const db = require("./db/connection.js");
 const routes = require("./routes/game-routes");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(routes);
+app.use(express.static("public"));
+app.use("/api", routes);
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
